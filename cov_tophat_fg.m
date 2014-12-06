463e1153c786fe8f1a722cd2e0a6b1d1a36a5f96
digirak <raknath@gmail.com>
2014-12-06 19:22:54 +0800

    w

--- cov_tophat_fg.m
+++ cov_tophat_fg.m
@@ -1,11 +1,14 @@
 clear;%%clear all
-freqs=[1:100]
+N=10000
+freqs=[1:N]
 freq_obs=150.0%%MHz
-df=10;
+df=1e+6;
 freqs=df*freqs;
-x=randn(1,100)%random distribution of the x
+x=(randn(1,N))%random distribution of the x
+%%l=acos(1:360)
+%%m=sqrt(abs(abs(x).^2-abs(l).^2))
 c=3.0e+8%%speed of light
-k=abs(fft(x))*c/freq_obs;%%observing frequency/central freq
+k=abs((x))*c/freq_obs;%%observing frequency/central freq
 alpha=10;%%fudge factor
 beta=0.67;
 gamma=0;
@@ -17,4 +20,13 @@ term1=alpha/(3-beta);
 term2=(S/S_max).^beta;
 term3=besselj(1,2*pi*k.*f_nu)./(f_nu.*k)%%sJ_1(2pif(nu)k)/kf(nu)
 C_fg=term1.*term2.*term3
+ft=fft(C_fg)
+
+blacknutt=0.3635819-0.4891775.*cos(2*pi.*[1:N]/(N-1))+0.1365995.*cos(4*pi.*[1:N]/(N-1))+0.0106411.*cos(6*pi.*[1:N]/(N-1))
+win_rect=(fir1(N-1,[0.25,0.75],'bandpass',rectwin(N)))
+win_black=(fir1(N-1,[0.25,0.75],'bandpass',blackman(N)));    % FIR filter
+win_bn=(fir1(N-1,[0.25,0.75],'bandpass',blacknutt));    % FIR filter
+out_rect=C_fg.*win_rect;
+out_black=C_fg.*win_black;
+out_bn=C_fg.*win_bn
 
