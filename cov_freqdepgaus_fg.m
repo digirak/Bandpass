a0326e65a7ca480c9895906348668fb13b733cd0
digirak <raknath@gmail.com>
2014-12-30 17:07:21 +0800

    Massive Change

--- cov_freqdepgaus_fg.m
+++ cov_freqdepgaus_fg.m
@@ -1,26 +1,87 @@
 clear;
-N=1000
-freqs=[1:N]
-freq_obs=150.0*1e+6%%MHz
-df=1e+6;
-freqs=df*freqs;
-x=randn(1,N)%random distribution of the x
+freqs_st=167e+6
+df=80e+3
+N=(197e+6-freqs_st)/df
+freqs=freqs_st+[0:N-1]*df
+freq_obs=freqs
+freq_base=192.7e+6
+dx=(600-10.)/N
+x=10+[0:N-1]*dx
 c=3.0e+8%%speed of light
-%%c=1
-k=abs((x))*c/freq_obs;%%observing frequency/central freq
-alpha=10;%%fudge factor
+alpha=1;%%fudge factor
 beta=0.67;
 gamma=0;
-S_max=1
+S_max=3
 S=1
-f_nu=(freqs-freq_obs)%%/freq_obs;
+for j=1:1
+	freq_obs(j)=freq_base
+k=((x))*freq_base/c;%%observing frequency/central freq
+f_nu=(freqs-freq_obs(j))/freq_base;
+f_nu(find(f_nu==0))=df/freq_base
 term1=alpha/(3-beta);
-%%term2=(S_max).^(3-beta)/S.^(-beta); %%use this later
-term2=(S/S_max).^beta;
+term2=(S_max).^(3-beta)/S.^(-beta); %%use this later
+%%term2=(S/S_max).^beta;
 epsilon=0.42;
 D=4;
+
 sigma=epsilon*c./(freqs*D);
-term3=pi*c.^2*epsilon.^2./(D*(freqs.^2+freq_obs.^2));
-terme=exp(-k.^2*c.^2.*f_nu.^2*epsilon.^2)%./(4*(freqs.^2+freq_obs.^2)*D.^2)
-C_fg=term1.*term2.*term3.*terme
+term3=pi*c.^2*epsilon.^2./(D*(freqs.^2+freq_obs(j).^2));
+
+denom=(4*(freqs.^2+freq_obs(j).^2)*D.^2)
+terme=exp((-c.^2.*(transpose(f_nu)*(k)).^2*epsilon.^2)/(4*(freqs.^2+freq_obs(j).^2)*D.^2))
+
+%C_fg(j,:)=term1.*term2.*term3.*terme
+C_fg=term1*term2*(terme*term3)
+end
+%F=fft(C_fg,[],1)
+%Fdag=ifft(C_fg,[],1)
+
+f=fft(C_fg,[],2)
+fdag=ifft(C_fg,[],2)
+
+%kk=F*C_fg*Fdag
+kkp=f*C_fg*fdag
+imagesc(log(abs(kkp(:,220:350))))
+set(gca,'YDir','normal')
+colorbar
+%for i=1:N
+%	F(i,:)=fft(C_fg(i,:));
+%	f(:,i)=fft(C_fg(:,i))
+%	Fdag(i,:)=(ifft(C_fg(i,:)));
+%	fdag(:,i)=ifft(C_fg(:,i));
+%end
+%%kk=Fdag*(C_fg)*F
+blacknutt=0.3635819-0.4891775.*cos(2*pi.*[1:N]/(N-1))+0.1365995.*cos(4*pi.*[1:N]/(N-1))+0.0106411.*cos(6*pi.*[1:N]/(N-1))
+win_rect=(fir1(N-1,0.03,'low',rectwin(N)))
+win_black=(fir1(N-1,0.03,'low',blackman(N)));    % FIR filter
+win_bn=(fir1(N-1,0.03,'low',blacknutt));    % FIR filter
+win_kaiser=(fir1(N-1,0.020,'low',kaiser(N,10)));
+%win_gauss=(fir1(N-1,0.020,'low',gausswin(N,20)))
+
+win_gauss=(fir1(N-1,[0.012,0.020],'bandpass',gausswin(N,20)))
+%out_rect=abs(ft).*(win_rect);
+%out_black=abs(ft).*(win_black);
+%out_bn=abs(ft).*(win_bn)
+%%loglog(abs(out_black))
+rect=freqz(win_rect)
+black=freqz(win_black)
+bn=freqz(win_bn)
+kai=freqz(win_kaiser)
+gau=freqz(win_gauss)
+
+for i=1:N
+	%rect_out(:,i)=kkp(:,i).*abs(rect(1:375))
+	%black_out(:,i)=kkp(:,i).*abs(black(1:375))
+	%bn_out(:,i)=kkp(:,i).*abs(bn(1:375))
+	%kaiser_out(:,i)=kkp(:,i).*abs(kai(1:375))
+	gauss_out(:,i)=kkp(:,i).*abs(gau(1:375))
+	
+end
+save('gauss-kaiser.mat')
+
+%ax=gca
+%hold(ax,'on')
+%loglog(abs(out_rect),'g')
+%loglog(abs(out_bn),'r')
+%legend('Blackman-Harris','rectangular','Blackman-nutall','location','SouthWest')
 
--- cov_tophat_fg.m
+++ cov_tophat_fg.m
@@ -1,32 +1,98 @@
 clear;%%clear all
-N=10000
-freqs=[1:N]
-freq_obs=150.0%%MHz
-df=1e+6;
-freqs=df*freqs;
-x=(randn(1,N))%random distribution of the x
-%%l=acos(1:360)
-%%m=sqrt(abs(abs(x).^2-abs(l).^2))
-c=3.0e+8%%speed of light
-k=abs((x))*c/freq_obs;%%observing frequency/central freq
-alpha=10;%%fudge factor
+freqs_st=167e+6
+df=80e+3
+%N=100
+N=(197e+6-freqs_st)/df
+freq_obs=freqs_st+[0:N-1]*df
+%freq_obs=freq_obs+80e+3
+%freqs=[1:N]*1e+6
+%freqs=[100:355]
+%freqs=[1:600]
+%freq_obs=[100:355]*1e+6%MHza
+freqs=freq_obs
+%%freq_obs=fliplr(freq_obs)
+%freq_base=[100:500]
+%freq_base=(167+(197-167.)/2)*1e+6
+%freq_base=150e+6
+dx=1*(600-10.)./N
+x=10+[0:N-1]*dx
+for j=1:1%numel(freq_obs)
+c=3.0e+8
+freq_obs(j)=192.7e+6
+%freq_base=freq_obs(j)
+freq_base=freqs_st
+k=x*freq_base/c;%%observing frequency/central freq
+alpha=1;%%fudge factor
 beta=0.67;
 gamma=0;
 S_max=1
 S=1;%%fudge factor 2
-f_nu=(freqs-freq_obs)/freq_obs;
+%%if(freq_obs(j)==freqs(j))
+%%	f_nu=1e-10
+%%else
+	f_nu=(freqs-freq_obs(j))/freq_base;
+%	for i=1:N
+%		if(freqs(i)==freq_obs(j))
+%			f_nu(i)=80e+3/freq_base;
+%		else
+%			f_nu(i)=(freqs(i)-freq_obs(j))/freq_base
+%		end
+%	end
+%end
+f_nu(find(f_nu==0))=df/freq_base
 term1=alpha/(3-beta);
-%%term2=(S_max).^(3-beta)/S.^(-beta); %%use this later
-term2=(S/S_max).^beta;
-term3=besselj(1,2*pi*k.*f_nu)./(f_nu.*k)%%sJ_1(2pif(nu)k)/kf(nu)
-C_fg=term1.*term2.*term3
-ft=fft(C_fg)
+term2=(S_max).^(3-beta)/S.^(-beta); %%use this later
+term3=besselj(1,2.*pi.*transpose(f_nu)*k)./(transpose(f_nu)*k)%%sJ_1(2pif(nu)k)/kf(nu)
 
+end
+C_fg=term1*term2*term3
+%C_fg(isnan(C_fg))=0.0
+
+%for j=1:N 
+%	F(j,:)=fft(C_fg(j,:));
+%	f(:,j)=fft(C_fg(:,j))
+%	Fdag(j,:)=(ifft((C_fg(j,:))));
+%	fdag(:,j)=ifft(C_fg(:,j))
+%end
+%%F=fft(C_fg,[],1)
+%%Fdag=ifft(C_fg,[],1)
+
+f=fft(C_fg,[],2)
+fdag=ifft(C_fg,[],2)
+
+%%kk=F*C_fg*Fdag
+kkp=f*C_fg*fdag
+%imagesc(log(abs(kkp(50:375,200:350))))
+%set(gca,'YDir','normal')
+%colorbar
 blacknutt=0.3635819-0.4891775.*cos(2*pi.*[1:N]/(N-1))+0.1365995.*cos(4*pi.*[1:N]/(N-1))+0.0106411.*cos(6*pi.*[1:N]/(N-1))
-win_rect=(fir1(N-1,[0.25,0.75],'bandpass',rectwin(N)))
-win_black=(fir1(N-1,[0.25,0.75],'bandpass',blackman(N)));    % FIR filter
-win_bn=(fir1(N-1,[0.25,0.75],'bandpass',blacknutt));    % FIR filter
-out_rect=C_fg.*win_rect;
-out_black=C_fg.*win_black;
-out_bn=C_fg.*win_bn
+win_rect=(fir1(N-1,[0.093,0.27],'bandpass',rectwin(N)))
+win_black=(fir1(N-1,[0.093,0.27],'bandpass',blackman(N)));    % FIR filter
+win_bn=(fir1(N-1,[0.093,0.27],'bandpass',blacknutt));    % FIR filter
+win_kaiser=(fir1(N-1,[0.10,0.23],'bandpass',kaiser(N,10)));    % FIR filter
+win_gauss=(fir1(N-1,[0.1,0.23],'bandpass',gausswin(N,20)))
+rect=freqz(win_rect)
+black=freqz(win_black)
+bn=freqz(win_bn)
+kai=freqz(win_kaiser)
+gau=freqz(win_gauss)
+'Created Windows'
+for i=1:N
+%	rect_out(:,i)=kkp(:,i).*rect(1:375)
+%	black_out(:,i)=kkp(:,i).*black(1:375)
+%	bn_out(:,i)=kkp(:,i).*bn(1:375)
+%kaiser_out(:,i)=kkp(:,i).*abs(kai(1:375))
+	gauss_out(:,i)=kkp(:,i).*abs(gau(1:375))
+end
 
+save('tophat.mat')
+imagesc(log(abs(kaiser_out(78:116,150:320))))
+%out_rect=abs(ft).*(win_rect);
+%out_black=abs(ft).*(win_black);
+%out_bn=abs(ft).*(win_bn)
+%loglog(abs(out_black))
+%ax=gca
+%hold(ax,'on')
+%loglog(abs(out_rect),'g')
+%loglog(abs(out_bn),'r')
+%legend('Blackman-Harr6is','rectangular','Blackman-nutall','location','SouthWest')
